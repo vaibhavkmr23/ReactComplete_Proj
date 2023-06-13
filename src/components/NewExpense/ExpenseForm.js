97247d1d475c34356d3dce040b603db1bf813829
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2023-06-13 18:32:27 +0530

    Working with multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,14 @@ const ExpenseForm = () => {
   const titleChangeHandler = (e) => {
     console.log(e.target.value);
   };
+
+  const amountChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
@@ -14,11 +22,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__controls">
           <label>Amount</label>
-          <input type="number" min={0.01} step={0.01} />
+          <input
+            type="number"
+            min={0.01}
+            step={0.01}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input type="date" min={"2019-01-01"} max={"2021-01-01"} />
+          <input
+            type="date"
+            min={"2019-01-01"}
+            max={"2021-01-01"}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
